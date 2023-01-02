ee179eef453c27fa6abc7548df1f1d42cdba7aab
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-01-03 00:23:42 +0300

    Added a VStack and Color ModeChanger Button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import { useState, useEffect, createContext } from 'react';
+import { IconButton, useColorMode, VStack } from '@chakra-ui/react';
+import { FaSun, FaMoon } from 'react-icons/fa';
 
 function App() {
-  return (
-    <div className="App">
+  	// chakraui color mode
+  const { colorMode, setColorMode } = useColorMode();
 
-    </div>
+
+
+  return (
+    <VStack p={5} mx="auto" maxW={{ base: '90vw', sm: '80vw', md: '70vw', lg: '600px' }}>
+
+    <IconButton 
+    			icon={colorMode === 'light' ? <FaSun /> : <FaMoon />}
+          alignSelf="flex-end"
+          size="lg"
+          isRound
+          onClick={setColorMode}
+    />
+
+
+    </VStack>
   );
 }
 
