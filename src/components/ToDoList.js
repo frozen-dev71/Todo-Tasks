1745b6ff95caa920706e33e630648e03154e31d2
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2023-01-03 00:57:23 +0300

    Added AddToTask Component

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,7 +1,16 @@
 import { EditTodo } from "../App";
 import { useContext } from "react";
+import {
+  Box,
+  VStack,
+  HStack,
+  Stack,
+  Text,
+  IconButton,
+  Badge,
+  Divider,
+} from "@chakra-ui/react";
 import { FaTrash, FaEdit } from "react-icons/fa";
-import { Box, VStack, HStack, Stack, Text, IconButton, Badge, Divider } from "@chakra-ui/react";
 
 export default function TodoList({ todos, handleRemoveTodo }) {
   const [, setEditTodo] = useContext(EditTodo); // global state
@@ -20,57 +29,64 @@ export default function TodoList({ todos, handleRemoveTodo }) {
     );
   }
 
-  return( 
-  <Box minW="100%" overflowY="auto" className="editScrollY">
-		<VStack p="1" minW="100%" alignItems="stretch" maxH="55vh">
-				{todos.map(function(todo) {
-					return (
-						<Stack
-							key={todo.id}
-							p="1"
-							borderRadius="lg"
-							border="1px"
-							borderColor={todo.priority ? 'red.300' : 'gray.300'}
-						>
-							<Text
-								as="h3"
-								fontWeight="bold"
-								textTransform="capitalize"
-								color={todo.priority ? 'red.500' : ''}
-							>
-								{todo.title}
-							</Text>
-							{todo.detail && <Divider marginTop="4px !important" borderColor="gray.300" />}
-							<Text marginTop="4px !important">{todo.detail}</Text>
-							<HStack
-								marginTop="0px !important"
-								textAlign="right"
-								borderRadius="lg"
-								padding="1"
-								w="100%"
-								justifyContent="space-between"
-							>
-								<Badge colorScheme={todo.priority ? 'red' : 'gray'} px="1" borderRadius="md">
-									{todo.priority ? 'High' : 'Normal'}
-								</Badge>
-								<HStack spacing="1">
-									<IconButton
-										color="blue.500"
-										icon={<FaEdit />}
-										onClick={() => handleEditTodo(todo.id)}
-										size="sm"
-									/>
-									<IconButton
-										color="red.600"
-										icon={<FaTrash />}
-										onClick={() => handleRemoveTodo(todo.id)}
-										size="sm"
-									/>
-								</HStack>
-							</HStack>
-						</Stack>
-					);
-				})}
-			</VStack>
-  </Box>);
+  return (
+    <Box minW="100%" overflowY="auto" className="editScrollY">
+      <VStack p="1" minW="100%" alignItems="stretch" maxH="55vh">
+        {todos.map(function (todo) {
+          return (
+            <Stack
+              key={todo.id}
+              p="1"
+              borderRadius="lg"
+              border="1px"
+              borderColor={todo.priority ? "red.300" : "gray.300"}
+            >
+              <Text
+                as="h3"
+                fontWeight="bold"
+                textTransform="capitalize"
+                color={todo.priority ? "red.500" : ""}
+              >
+                {todo.title}
+              </Text>
+              {todo.detail && (
+                <Divider marginTop="4px !important" borderColor="gray.300" />
+              )}
+              <Text marginTop="4px !important">{todo.detail}</Text>
+              <HStack
+                marginTop="0px !important"
+                textAlign="right"
+                borderRadius="lg"
+                padding="1"
+                w="100%"
+                justifyContent="space-between"
+              >
+                <Badge
+                  colorScheme={todo.priority ? "red" : "gray"}
+                  px="1"
+                  borderRadius="md"
+                >
+                  {todo.priority ? "High" : "Normal"}
+                </Badge>
+                <HStack spacing="1">
+                  <IconButton
+                    color="blue.500"
+                    icon={<FaEdit />}
+                    onClick={() => handleEditTodo(todo.id)}
+                    size="sm"
+                  />
+                  <IconButton
+                    color="red.600"
+                    icon={<FaTrash />}
+                    onClick={() => handleRemoveTodo(todo.id)}
+                    size="sm"
+                  />
+                </HStack>
+              </HStack>
+            </Stack>
+          );
+        })}
+      </VStack>
+    </Box>
+  );
 }
